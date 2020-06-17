ba7f260ffbd8f6d1abedf866545477b6aa36af57
faisal-510 <66562768+faisal-510@users.noreply.github.com>
2020-06-17 15:36:20 +0300

    احتاج مساعدة، عقب المحاظرة راح اظبطة

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,35 +38,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Faisal"
+var lastName1 = "Al Ahmad"
+var email1 = "alahmadfaisal@gmail.com"
+var phoneNumber1 = "90992424"
+var age1 = 16
+var country1 = "kuwait"
+var area1 = "Hatten"
+var password = "Faisal123"
+var isKuwaiti = Bool(true)
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "Saleh"
+var lastName2 = "AL Mutawa"
+var email2 = "Bo9lo7@gmail.com"
+var phoneNumber2 = "99876789"
+var age2 = 16
+var country2 = "kuwait"
+var area2 = "Bayan"
+var Password = "Salehh2314"
+var IsKuwaiti = Bool(true)
 
 //Person 3
-
-
-
-
-
-
-
+var firstName3 = "Ahmad"
+var lastName3 = "AL Jasem"
+var email3 = "Bojasem@gmail.com"
+var phoneNumber3 = "65672835"
+var age3 = 16
+var country3 = "Kuwait"
+var area3 = "Yarmouk"
+var password = "ahmad2004"
+var IsKuwaiti = Bool(true)
 
 
 /*
@@ -82,3 +84,9 @@ var isKuwaiti = false
 
  
  */
+
+
+
+
+
+
